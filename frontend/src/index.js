2e0cf8ab8b8ec9f178661ffdc53e10df0e5c2192
JoaquinHiguitaJL27 <joaquin050218@gmail.com>
2020-09-25 14:14:48 -0500

    se agregan más complementeos para js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+	import $ from 'jquery';
+	import Popper from 'popper.js';
+	import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './Paginas/App';
 import Start from './Paginas/StartP';
 import Inicio from './Paginas/Inicio';
