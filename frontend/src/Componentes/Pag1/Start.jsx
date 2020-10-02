81d7e26b16ff8cf0fb794a54dace4898858074a5
david0202282 <davidcastanedaquintero@gmail.com>
2020-10-02 18:09:32 -0500

    Intentando arreglar el proyecto con la señorita Sarai

--- frontend/src/Componentes/Pag1/Start.jsx
+++ frontend/src/Componentes/Pag1/Start.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo1 from '../img/Logosincachito.png';
-import fondo from '../img/fondo.png';
 import './StartE.css';
 
 
@@ -12,7 +11,7 @@ function Start(){
        <h1 className="Bienvenido mt-5 mb-5">Bienvenido a PetSociety</h1>
 
        
-      <img className="imagenbienvenido mt-3 mb-4" src={Logo1}/>
+      <img alt="responsive" className="imagenbienvenido mt-3 mb-4" src={Logo1}/>
     
 
        <div className="Botonbienvenido mt-5 mb-5">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+/*
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './Paginas/App';
 import Start from './Paginas/StartP';
 import Inicio from './Paginas/Inicio';
@@ -14,11 +15,11 @@ import ConsejosP from './Paginas/ConsejosP';
 import InformacionP from './Paginas/InformacionP';
 import InformacionG from './Paginas/InformacionG';
 import AdopcionG from './Paginas/AdopcionG';
-import AdopcionP from './Paginas/AdopcionP';
+
 import Configuraciones from './Paginas/Configuraciones';
 import ComunidadPet from './Paginas/ComunidadPet';
-
-
+*/
+import AdopcionP from './Paginas/AdopcionP';
 
 
 ReactDOM.render(
