f5cb27b3e7f1e408f67829376d72eba1242a3edb
LoperaSebas <sebastianparralopera351@gmail.com>
2020-11-27 13:06:32 -0500

    Base de datos ingresada...

--- frontend/src/Componentes/Pag3/Registro.jsx
+++ frontend/src/Componentes/Pag3/Registro.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo1 from '../img/Logosincachito.png';
 import './RegistroE.css';
 import {Link} from 'react-router-dom'; 
+import { naxios } from '../Utilities';
 
-function Registro() {
+class Registro extends Component {
+  constructor(props) {
+    //Inicialización del estado y llamado de props
+    super(props);
+    this.state ={
+        losRegistros:{
+          nombres: '',
+          apellidos: '',
+          correo_electronico: '',
+          fecha_de_nacimiento: '',
+          edad: '',
+          contrasena: '',
+          rep_contrasena: '',
+          tipo_identidad: '',
+          num_identidad: '',
+          num_celular: '',
+          num_telefono: '',
+          direccion: '',
+          add_direccion: '',
+          barrio: '',
+          codigo_postal: ''
+        }
+    }
+  }
+
+  PostPerfil=async()=>{
+    await naxios.post('https://BaseDeDatosPetSociety.loperasebas.repl.co/api/nuevo-perfil', this.state.losRegistros)
+    .then(res=>{
+    }).catch(error=>{
+      console.log(error.message)
+    })
+  }
+
+  handleChange=async (e) =>{
+    e.persist();
+    await this.setState({
+      losRegistros:{
+        ...this.state.losRegistros,
+        [e.target.name]: e.target.value 
+      }
+    })
+    console.log(this.state.losRegistros)
+  }
+
+  render(){
   return (
     <>
         <h1 className="tituloregistro mt-4">Registro de Petsociety</h1>
@@ -30,14 +75,14 @@ function Registro() {
       <div className="form-row">
           <div className="col-6 mb-3">
             <label for="validationCustom01">Nombres</label>
-            <input type="text" className="form-control" id="validationCustom01" value="" required></input>
+            <input type="text" className="form-control" id="validationCustom01" name="nombres" onChange={this.handleChange} value={this.state.losRegistros.nombres} required></input>
             <div className="valid-feedback">
               Looks good!
       </div>
           </div>
           <div className="col-6 mb-3">
             <label for="validationCustom02">Apellidos</label>
-            <input type="text" className="form-control" id="validationCustom02" value="" required></input>
+            <input type="text" className="form-control" id="validationCustom02" name="apellidos" onChange={this.handleChange} value={this.state.losRegistros.apellidos} required></input>
             <div className="valid-feedback">
               Looks good!
       </div>
@@ -46,32 +91,32 @@ function Registro() {
 
         <div className="form-group">
           <label for="inputEmail4">Correo electronico</label>
-          <input type="email" className="form-control" id="inputEmail4"></input>
+          <input type="email" className="form-control" id="inputEmail4" name="correo_electronico" onChange={this.handleChange} value={this.state.losRegistros.correo_electronico}></input>
         </div>
         <div className="form-row">
         <div className="form-group col-6">
           <label for="inputAge">Fecha de nacimiento</label>
-          <input type="text" className="form-control" id="inputAge"></input>
+          <input type="text" className="form-control" id="inputAge" name="fecha_de_nacimiento" onChange={this.handleChange} value={this.state.losRegistros.fecha_de_nacimiento}></input>
           <small>
             dia/mes/año
         </small>
         </div>
         <div className="form-group col-6">
           <label for="inputAge">Edad</label>
-          <input type="text" className="form-control" id="inputAge" placeholder="40"></input>
+          <input type="text" className="form-control" id="inputAge" placeholder="40" name="edad" onChange={this.handleChange} value={this.state.losRegistros.edad}></input>
         </div>
         </div>
         <div className="form-row">
          <div className="form-group col-6">
         <label for="inputPassword5">Contraseña</label>
-        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock"></input>
+        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" name="contrasena" onChange={this.handleChange} value={this.state.losRegistros.contrasena}></input>
         <small id="passwordHelpBlock" className="form-text text-muted">
         Su contraseña debe tener entre 8 y 20 caracteres, contener letras y números, y no debe contener espacios, caracteres especiales ni emoji.
 </small>
 </div>
 <div className="form-group col-6">
         <label for="inputPassword5">Repita la contraseña</label>
-        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock"></input>
+        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" name="rep_contrasena" onChange={this.handleChange} value={this.state.losRegistros.rep_contrasena}></input>
         <small id="passwordHelpBlock" className="form-text text-muted">
         Su contraseña debe tener entre 8 y 20 caracteres, contener letras y números, y no debe contener espacios, caracteres especiales ni emoji.
 </small>
@@ -80,8 +125,8 @@ function Registro() {
 <div className="form-row">
         <div className="form-group col-6">
           <label for="Identification">Tipo de identificación</label>
-          <select className="form-control" id="Identification" required="">
-          <option value="">Selecciona un tipo</option>
+          <select className="form-control" id="Identification" required="" name="tipo_identidad" onChange={this.handleChange} value={this.state.losRegistros.tipo_identidad}>
+          <option selected>Selecciona un tipo</option>
             <option>Tarjeta de identidad</option>
             <option>Cedula ciudadana</option>
             <option>Adulto sin identidad</option>
@@ -91,34 +136,34 @@ function Registro() {
         </div>
         <div className="form-group col-6">
           <label for="inputAddress">Número identificación</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="1011390733"></input>
+          <input type="text" className="form-control" id="inputAddress" placeholder="1011390733" name="num_identidad" onChange={this.handleChange} value={this.state.losRegistros.num_identidad}></input>
         </div>
 </div>
 <div className="form-row">
         <div className="form-group col-6">
           <label for="inputAddress">Celular</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="3104109270"></input>
+          <input type="text" className="form-control" id="inputAddress" placeholder="3104109270" name="num_celular" onChange={this.handleChange} value={this.state.losRegistros.num_celular}></input>
         </div>
         <div className="form-group col-6">
           <label for="inputAddress">Telefono <span className="text-muted">(Opcional)</span></label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="5884735"></input>
+          <input type="text" className="form-control" id="inputAddress" placeholder="5884735" name="num_telefono" onChange={this.handleChange} value={this.state.losRegistros.num_telefono}></input>
         </div>
   </div>
   <div className="form-row">
   <div className="mb-3 col-md-4 ola">
-          <label for="address">Dirección <span className="text-muted">(Opcional)</span> </label>
-          <input type="text" className="form-control" id="address" placeholder="Cra. 40 #12-34" required="" />
+          <label for="address">Dirección <span className="text-muted"></span> </label>
+          <input type="text" className="form-control" id="address" placeholder="Cra. 40 #12-34" name="direccion" onChange={this.handleChange} value={this.state.losRegistros.direccion} />
           <div className="invalid-feedback">
             Por favor ingresar tu dirección para enviar el/los productos.
           </div>
           </div>
           <div class="mb-3 col-md-4">
           <label for="address2">Piso o Apartamento<span class="text-muted ml-1">(Opcional)</span></label>
-          <input type="text" class="form-control" id="address2" placeholder="(Piso 1, 2, 3, etc.) o (Apto 101, 102, 201, etc.)" />
+          <input type="text" class="form-control" id="address2" placeholder="(Piso 1, 2, 3, etc.) o (Apto 101, 102, 201, etc.)" name="add_direccion" onChange={this.handleChange} value={this.state.losRegistros.add_direccion} />
         </div>
           <div className="form-group col-md-3 ola1">
           <label for="inputCity">Barrio</label>
-          <input type="text" className="form-control" id="inputCity"></input>
+          <input type="text" className="form-control" id="inputCity" name="barrio" onChange={this.handleChange} value={this.state.losRegistros.barrio}></input>
         </div> 
        
 </div>
@@ -145,17 +190,17 @@ function Registro() {
           </div>
           <div class="col-md-3 mb-3">
             <label for="zip">Código Postal<span class="text-muted ml-1">(Opcional)</span></label>
-            <input type="text" class="form-control" id="zip" placeholder="054448" required="" />
+            <input type="text" class="form-control" id="zip" placeholder="054448" name="codigo_postal" onChange={this.handleChange} value={this.state.losRegistros.codigo_postal} />
           </div>
         </div>
      
         <div classNameName="Boton2">
-        <Link to="/abrebocas" role="button" className="btn btn-primary btn-lg">Registrarse</Link>
+        <Link to="/abrebocas" role="button" className="btn btn-primary btn-lg" onClick={this.PostPerfil}>Registrarse</Link>
               </div>
       </form>
       </div>   
     </>
   );
-}
+}}
 export default Registro;
 
--- frontend/src/Paginas/Inicio.js
+++ frontend/src/Paginas/Inicio.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Inicio from '../Componentes/Pag2/Inicio';
 import './App.css';
-
 
 export default function Inicioa() {
   return ( 
